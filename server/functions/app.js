1fb7619972504bc988cea6c27e3f31c5dbcbfea2
Maximusdev1313 <maximusdev1313@gmail.com>
2024-08-10 01:01:11 +0500

    add new html

--- server/functions/app.js
+++ server/functions/app.js
@@ -1,32 +1,34 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors')
-const app = express()
+const cors = require('cors');
+const app = express();
 const serverless = require("serverless-http");
 
 const router = express.Router();
 
-const port = process.env.PORT || 5000
-process.env.TZ = "Asia/Tashkent"
+const port = process.env.PORT || 5000;
+process.env.TZ = "Asia/Tashkent";
 
-app.use(cors({ origin: ['https://razzoq.uz', 'http://localhost:5173', 'https://razzoq.netlify.app', 'http://localhost:5174', 'https://justuz.netlify.app',] }))
+app.use(cors({ origin: ['https://razzoq.uz', 'http://localhost:5173', 'https://razzoq.netlify.app', 'http://localhost:5174', 'https://justuz.netlify.app'] }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('uploads'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('uploads'));
 
-// database connection
+// Log the DB_URI to check if it's loaded correctly
+console.log('DB_URI:', process.env.DB_URI);
+
+// Database connection
 mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
     useUnifiedTopology: true,
+    useNewUrlParser: true,
 }).then(() => {
-    console.log("Database connected")
-}).catch(err => console.log(err))
+    console.log("Database connected");
+}).catch(err => console.log(err));
 
-//routes
-router.use('/api/persons', require('../urls/personUrl'))
-
+// Routes
+router.use('/api/persons', require('../urls/personUrl'));
 
 app.use(`/.netlify/functions/app`, router);
 
